b309ee3387c917008b1d9139a2b5702a76229e72
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-02-10 14:59:55 +0100

    formatter toegevoegd

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,14 +3,25 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		vim.keymap.set("n", "<C-s>", ":Neotree toggle<CR>")
-		local config = require("nvim-treesitter.configs")
-		config.setup({
+		local config = require "nvim-treesitter.configs"
+		config.setup {
 			ensure_installed = {
-				"python", "c", "css", "csv", "dockerfile", "html", "java",
-				"sql", "yaml", "json", "lua", "javascript"
+				"python",
+				"c",
+				"css",
+				"csv",
+				"dockerfile",
+				"html",
+				"java",
+				"sql",
+				"yaml",
+				"json",
+				"lua",
+				"javascript",
 			},
+			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
-		})
-	end
+		}
+	end,
 }
--- lua/vim_settings.lua
+++ lua/vim_settings.lua
@@ -1,7 +1,7 @@
 vim.o.expandtab = false      -- Gebruik tabs in plaats van spaces
-vim.o.tabstop = 4            -- Breedte van een tab is 4 spaces
-vim.o.shiftwidth = 4         -- Indentering gebruikt 4 spaces per niveau
-vim.o.softtabstop = 4        -- Backspace werkt correct met tabs als 4 spaces
+vim.o.tabstop = 2            -- Breedte van een tab is 2 spaces
+vim.o.shiftwidth = 2         -- Indentering gebruikt 2 spaces per niveau
+vim.o.softtabstop = 2        -- Backspace werkt correct met tabs als 2 spaces
 vim.o.smarttab = true        -- Zorgt ervoor dat tab-toets rekening houdt met shiftwidth
 vim.o.number = true
 vim.opt.relativenumber = true
