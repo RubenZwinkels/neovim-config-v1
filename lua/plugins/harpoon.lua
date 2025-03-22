240b451497891de5bcd477434689b757931821f0
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-03-22 14:58:25 +0100

    [harp] keybinds aangepast

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,13 +2,15 @@ return {
   "ThePrimeagen/harpoon",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   config = function()
-    local harpoon = require("harpoon")
-    local harpoon_telescope = require("config.harpoon_telescope")
+    local harpoon = require "harpoon"
+    local harpoon_telescope = require "config.harpoon_telescope"
 
     harpoon.setup()
 
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = "Harpoon add file" })
-    vim.keymap.set("n", "<leader>fh", harpoon_telescope, { desc = "Telescope Harpoon" })
-    vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon file 1" })
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = "Harpoon add file" })
+    vim.keymap.set("n", "<leader>ht", harpoon_telescope, { desc = "Telescope Harpoon" })
+    for i = 1, 9 do
+      vim.keymap.set("n", "<leader>h" .. i, function() harpoon:list():select(i) end, { desc = "Harpoon file " .. i })
+    end
   end,
 }
