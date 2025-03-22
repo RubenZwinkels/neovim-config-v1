9147ef9a9f63ebdccddb30ff19ed03a1960cbc44
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-03-22 11:28:39 +0100

    [debug] [feat] dap basic funtions toegevoegd

---
+++ lua/plugins/debug.lua
@@ -0,0 +1,18 @@
+return {
+	"mfussenegger/nvim-dap",
+	config = function()
+		local dap = require "dap"
+
+		vim.keymap.set("n", "<leader> dc", function() dap.continue() end)
+		vim.keymap.set("n", "<leader> do", function() dap.step_over() end)
+		vim.keymap.set("n", "<leader di", function() dap.step_into() end)
+		vim.keymap.set("n", "<leader> dO", function() dap.step_out() end)
+		vim.keymap.set("n", "<Leader>db", function() dap.toggle_breakpoint() end)
+		vim.keymap.set("n", "<Leader>dB", function() dap.set_breakpoint() end)
+		vim.keymap.set("n", "<Leader>lp", function() dap.set_breakpoint(nil, nil, vim.fn.input "Log point message: ") end)
+		vim.keymap.set("n", "<Leader>dr", function() dap.repl.open() end)
+		vim.keymap.set("n", "<Leader>dl", function() dap.run_last() end)
+		vim.keymap.set({ "n", "v" }, "<Leader>dh", function() require("dap.ui.widgets").hover() end)
+	end,
+}
+
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,31 +1,45 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	config = function()
-		local wk = require "which-key"
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+        local wk = require "which-key"
+        local dap = require "dap"
 
-		wk.register({
-			l = {
-				name = "LSP", -- Groepeert LSP-acties onder <leader>l
-				h = { vim.lsp.buf.hover, "Hover Documentation" },
-				d = { vim.lsp.buf.definition, "Go to Definition" },
-				r = { vim.lsp.buf.rename, "Rename Symbol" },
-				f = { vim.lsp.buf.format, "Format Code" },
-				c = { "gcc", "Comment Line" },
-			},
-			t = {
-				name = "Telescope",
-				f = { require("telescope.builtin").find_files, "Find Files" },
-				n = { require("telescope.builtin").find_files, "FF NeoVim config" },
-			},
-			g = {
-				name = "Git",
-				d = { ":Gitsigns preview_hunk<CR>", "Show git diff" },
-			},
-		}, { prefix = "<leader>" })
+        wk.register({
+            l = {
+                name = "LSP",
+                h = { vim.lsp.buf.hover, "Hover Documentation" },
+                d = { vim.lsp.buf.definition, "Go to Definition" },
+                r = { vim.lsp.buf.rename, "Rename Symbol" },
+                f = { vim.lsp.buf.format, "Format Code" },
+                c = { "gcc", "Comment Line" },
+            },
+            t = {
+                name = "Telescope",
+                f = { require("telescope.builtin").find_files, "Find Files" },
+                n = { require("telescope.builtin").find_files, "FF NeoVim config" },
+            },
+            g = {
+                name = "Git",
+                d = { ":Gitsigns preview_hunk<CR>", "Show git diff" },
+            },
+            d = {
+                name = "Debugger",
+                c = { function() dap.continue() end, "Continue" },
+                o = { function() dap.step_over() end, "Step Over" },
+                i = { function() dap.step_into() end, "Step Into" },
+                O = { function() dap.step_out() end, "Step Out" },
+                b = { function() dap.toggle_breakpoint() end, "Toggle Breakpoint" },
+                B = { function() dap.set_breakpoint() end, "Set Breakpoint" },
+                lp = { function() dap.set_breakpoint(nil, nil, vim.fn.input "Log point message: ") end, "Set Log Point" },
+                r = { function() dap.repl.open() end, "Open REPL" },
+                l = { function() dap.run_last() end, "Run Last" },
+                h = { function() require("dap.ui.widgets").hover() end, "Hover Debug Info" },
+            }
+        }, { prefix = "<leader>" })
 
-		wk.register {
-			["<C-.>"] = { vim.lsp.buf.code_action, "Code Actions" },
-		}
-	end,
+        wk.register {
+            ["<C-.>"] = { vim.lsp.buf.code_action, "Code Actions" },
+        }
+    end,
 }
