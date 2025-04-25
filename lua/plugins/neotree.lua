99c4f207e91872f78c100719b3ed15c2bf304d38
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-04-25 11:47:43 +0200

    neotree- toont nu ook hidden files, waaronder dotfiles

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,13 +1,24 @@
-return	{
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-s>", ":Neotree toggle<CR>")
-		vim.keymap.set("n", "C", ":Neotree set_root<CR>") 
-end
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<C-s>", ":Neotree toggle<CR>")
+    vim.keymap.set("n", "C", ":Neotree set_root<CR>")
+
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,           -- Toon gefilterde items
+          hide_dotfiles = false,    -- Laat .dotfiles zien
+          hide_gitignored = false,  -- Laat gitignored files zien
+          show_hidden_count = true,
+        },
+      },
+    })
+  end,
 }
