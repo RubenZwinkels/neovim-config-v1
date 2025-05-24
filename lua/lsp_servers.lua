232c05029913d8e44b7dff115a74d39e66eb1119
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-05-24 10:05:47 +0200

    lsp omnisharp toegevoegd voor csharp development

--- lua/lsp_servers.lua
+++ lua/lsp_servers.lua
@@ -1,11 +1,13 @@
 return {
+
   "lua_ls", -- Lua
   "ast_grep", -- python, C, C#, C++, CSS, HTML, JS, rust, TS
   "pyright", -- volledige ondersteuning python
   "clangd", -- C
   "jdtls", -- Java
-	"ts_ls",
-	"eslint",
-	"html",
-	"volar",
+  "ts_ls",
+  "eslint",
+  "html",
+  "volar",
+  "omnisharp",
 }
