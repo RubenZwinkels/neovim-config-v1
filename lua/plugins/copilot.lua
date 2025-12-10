21c869e39ee2badfa5ed077fa66c81d5340174f8
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2025-12-10 16:05:58 +0100

    copilot 1 completion geven

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,17 +1,21 @@
 return {
-	{
-		"github/copilot.vim",
-	},
-	{
-  "folke/sidekick.nvim",
-  opts = {
-    -- add any options here
-    cli = {
-      default = "copilot",
-      mux = {
-        enabled = false,
+  {
+    "github/copilot.vim",
+    config = function()
+      -- Voorkom dat Copilot automatisch grote suggesties pusht
+      vim.g.copilot_auto_trigger = true -- subtiele inline suggesties
+      vim.g.copilot_suggestion_delay = 75 -- sneller maar klein
+    end,
+  },
+  {
+    "folke/sidekick.nvim",
+    opts = {
+      cli = {
+        default = "copilot",
+        mux = {
+          enabled = false,
+        },
       },
     },
   },
-}
 }
