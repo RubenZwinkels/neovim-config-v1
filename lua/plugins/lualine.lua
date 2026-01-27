787ddbd6ae082c6383c379fe7fb797d3c9a5e1ff
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2026-01-27 15:26:00 +0100

    ultra clean lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,38 +1,35 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "lewis6991/gitsigns.nvim",
+  },
   config = function()
-    local function search_count()
-      local sc = vim.fn.searchcount({recompute = 0}).total
-      if sc == 0 then return "" end
-      return "🔍" .. sc
+    local function blame_if_not_me()
+      local blame = vim.b.gitsigns_blame
+      if not blame or blame.author == "Ruben Zwinkels" then
+        return ""
+      end
+      return " " .. blame.author
     end
 
-    require('lualine').setup({
+    require("lualine").setup({
       options = {
-        theme = "gruvbox",        -- je kan hier dark-soft gebruiken als je custom theme hebt
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '|', right = '|' },
+        theme = "auto",   -- default look
         icons_enabled = true,
       },
       sections = {
-        lualine_a = { "mode" },   -- links
+        lualine_a = { "mode" },
         lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "diff", "diagnostics", search_count }, -- rechts
+        lualine_c = {},
+        lualine_x = {
+          { "diff" },
+          { "diagnostics" },
+          blame_if_not_me,
+        },
         lualine_y = {},
-        lualine_z = {}
+        lualine_z = {},
       },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {}
-      },
-      tabline = {},
-      extensions = {}
     })
-  end
+  end,
 }
