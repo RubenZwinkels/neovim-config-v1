184a0ba2f931537c08e2a61181b212d48efc2bb5
Ruben Zwinkels <rubenzwinkels05@gmail.com>
2026-01-27 15:14:11 +0100

    gruvbox thema instellen

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,12 +4,23 @@ return {
     lazy = false,
     priority = 1000,
     config = function() vim.cmd.colorscheme "tokyonight" end,
-		enabled=false
+    enabled = false,
   },
   {
     "RubenZwinkels/tokyonight",
     lazy = false,
     priority = 1000,
+		enabled = false,
     config = function() vim.cmd.colorscheme "tokyonight" end,
+  },
+ {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+		enabled = true,
+    config = function()
+      vim.o.background = "dark"
+      vim.g.gruvbox_contrast_dark = "soft"
+      vim.cmd.colorscheme "gruvbox"
+    end,
   },
 }
